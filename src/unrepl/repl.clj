59165c66ab545fb0130f81f22933390eae2584fc
Meikel Brandmeyer <mb@kotka.de>
2018-03-25 19:30:38 +0200

    Fix start-aux with missing start argument

--- src/unrepl/repl.clj
+++ src/unrepl/repl.clj
@@ -413,7 +413,7 @@
   (let [cl (.getContextClassLoader (Thread/currentThread))]
     (try
       (some->> session-id session :class-loader (.setContextClassLoader (Thread/currentThread)))
-      (start)
+      (start {})
       (finally
         (.setContextClassLoader (Thread/currentThread) cl)))))
 
